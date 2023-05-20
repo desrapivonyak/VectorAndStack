457336e6990bfd687d6cf48defb54a583c878b53
Tatev <34788695+tatevsrapivonyan@users.noreply.github.com>
2023-05-20 22:21:37 +0400

    Update Vector.cpp

--- Vector.cpp
+++ Vector.cpp
@@ -1,55 +1,28 @@
 #pragma once
-#include "Vector.h"
+#include <string>
 
 template <typename T>
-Vector<T>::Vector(): size (0), array(nullptr) {}
 
-template <typename T>
-void Vector<T>::set_size(const int num) {
-  this->size = num;
-}
+class Vector{
+public:
+  Vector();
+  ~Vector();
 
-template <typename T>
-int Vector<T>::get_size() const {
-  return this->size;
-}
+  void set_size(const int);
+  int get_size() const;
 
-template <typename T>
-void Vector<T>::set_capacity(const int num) {
-  this->capacity = num;
-}
+  void set_capacity(const int);
+  int get_capacity() const;
 
-template <typename T>
-int Vector<T>::get_capacity() const {
-  return this->capacity;
-}
+  void push_back(const T&);
+  void pop_back();
 
-template <typename T>
-void Vector<T>::push_back(const int a) {
-  if(size == capacity) {
-    capacity *= 2;
-  }
-  
-  this->array = new T[size];
-  T* new_arr = new int[size];
-  
-  for(int i = 0; i < size; ++i) {
-    new_arr[i] = this->array[i];
-  }
-  
-  delete [] this->array;
-  this->array = new_arr;
-  this->array[size++] = a;
-}
+  T& operator[](const int);
+protected:
+  int size;
+  int capacity;
+  T* array;
+};
 
-template <typename T>
-void Vector<T>::pop_back() {
-  this->size--;
-}
-
-template <typename T>
-int& Vector<T>::operator[](const int i) {
-  return this->array[i];
-}
-
-template class Vector<int>;
+extern template class Vector<int>;
+extern template class Vector<std::string*>;
